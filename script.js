81b8c5ab45b37723d75e4cbaab360ae304ccc721
Igor Lopes <igor_lopes90@yahoo.com.br>
2023-05-24 10:14:53 -0300

    Projeto pronto

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function calcular() {
+    let a = Number((document.querySelector('input.valor-a')).value);
+    let b = Number((document.querySelector('input.valor-b')).value);
+    let c = Number((document.querySelector('input.valor-c')).value);
+    let res = document.querySelector('p.res');
+    
+    let delta = (b ** 2) - (4 * a * c); //Δ = b² – 4ac
+    let x1 = (-b + (delta ** 0.5)) / (2 * a); // x' = (-b + √Δ) / (2a)
+    let x2 = (-b - (delta ** 0.5)) / (2 * a); // x" = (-b - √Δ) / (2a)
+    res.innerHTML = `<p>O valor de \u{0394} é ${delta}</p>`;
+
+    if (delta == 0) {
+        res.innerHTML += ' '
+    } else if (delta < 0) {
+        res.innerHTML += `</br>O valor de \u{0394} é negativo.</br>A equação não terá valores reais.`
+    } else {
+        res.innerHTML += `</br><p>O valor de x' é ${x1}</p>`;
+        res.innerHTML += `</br><p>O valor de x" é ${x2}</p>`;
+    }
+}
+
+function recalc() {
+    let a2 = document.querySelector('input.valor-a');
+    let b2 = document.querySelector('input.valor-b');
+    let c2 = document.querySelector('input.valor-c');
+    let res2 = document.querySelector('p.res');
+
+    a2.value = '';
+    b2.value = '';
+    c2.value = '';
+    res2.innerHTML = 'Resultado'
+}
